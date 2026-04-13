b9a5ed7ceef3853086bebbddf2c3c92cf706eb35
Jairo <jaironuneznogueira@gmail.com>
2026-04-13 17:52:54 +0200

    Test: getRadio() - verifica radio con valor determinado

--- src/test/java/com/dam/practicajunit/CirculoTest.java
+++ src/test/java/com/dam/practicajunit/CirculoTest.java
@@ -5,10 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CirculoTest {
 
+    // CASO A: Constructor vacío
     @Test
     public void testConstructorVacio() {
         Circulo c = new Circulo();
         assertEquals(0.0, c.getRadio());
+    }
+
+    // CASO B: Método getRadio()
+    @Test
+    public void testGetRadio() {
+        Circulo c = new Circulo(5);
+        assertEquals(5.0, c.getRadio());
     }
 
 }
